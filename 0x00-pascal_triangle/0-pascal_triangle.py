87734b5a16a62f859de73482275ba23aca0f7be2
meriem matnoussi <meriem.matnoussi123@gmail.com>
2024-03-07 07:40:55 -0800

    task 0

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+pascal_triangle
+"""
+
+
+def pascal_triangle(n):
+    """returns the pascal triangle"""
+
+    if n <= 0:
+        return []
+    triangle = [[1]]
+    for i in range(1, n):
+        row = [1]
+        last_row = triangle[-1]
+        for j in range(1, i):
+            row.append(last_row[j-1] + last_row[j])
+        row.append(1)
+        triangle.append(row)
+    return triangle
